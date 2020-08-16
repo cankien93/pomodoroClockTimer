7a965493a1dab84f5d6b83285e7add396766cb30
cankien <cankien93@gmail.com>
2020-08-16 10:00:24 +0900

    Updates

---
+++ precache-manifest.a682b7f248388c14e06aa1fafb9c5d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719543e4c36c90c408eef2cad99616f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f332c5fa8ddaa194448e",
+    "url": "/static/css/main.73ca4394.chunk.css"
+  },
+  {
+    "revision": "fa3820f89223a46fb4ef",
+    "url": "/static/js/2.fe173f38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe173f38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f332c5fa8ddaa194448e",
+    "url": "/static/js/main.07e62a77.chunk.js"
+  },
+  {
+    "revision": "89ac35b4ba8ca899f16a",
+    "url": "/static/js/runtime-main.7f7132dd.js"
+  },
+  {
+    "revision": "d2d9d8e7d232cfba2b6776e3ae2addbb",
+    "url": "/static/media/Little-Girl-Laughing-A1.d2d9d8e7.mp3"
+  }
+]);
